92c69ca295050880031e4c0532d4eb1dddcafd71
iamcodefoxx <71807189+iamcodefoxx@users.noreply.github.com>
2020-10-31 12:40:17 -0700

    Delete script.js

--- #4TheMessage/script.js
+++
@@ -1,7 +0,0 @@
-function getMessage(){
-
-  var message = document.getElementById("entry").value;
-  
-  document.getElementById("message").innerHTML = message;
-  document.getElementById("entry").value = "";
-}
